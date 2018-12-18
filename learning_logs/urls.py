230145b47c9375afd6c97cf98292e5dccbacba2e
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2018-12-18 18:20:25 +0100

    Added entries and editing ability

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,8 +12,14 @@ urlpatterns = [
     url(r'^topics/$', views.topics, name='topics'),
 
     # Strona z detalami danego tematu
-    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 
     # Strona do tworzenia nowego tematu
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # Strona do doawania nowych wpisów
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+
+    # Strona do edycji wpisów
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -37,3 +37,42 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Dodaj nowy wpis"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # Brak przekazanych danych, utwórz pusty formularz
+        form = EntryForm()
+    else:
+        # Dane POST przesłane, przetwórz dane
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                        args=[topic_id]))
+
+    context = {'topic' : topic, 'form' : form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edytuj istniejący wpis"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    topic_id = topic.id
+
+    if request.method != 'POST':
+        # Wstępne żądanie; wypełnij wcześniej istniejącym wpisem
+        form = EntryForm(instance=entry)
+    else:
+        # Dane POST przesłane, przetwórz dane
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic_id]))
+    context = {'entry' : entry, 'topic' : topic, 'form' : form}
+    return render(request, 'learning_logs/edit_entry.html', context)
